3d03d2b82e383a0ca35f05e01e7b9929884d1194
rochu36 <132965693+rochu36@users.noreply.github.com>
2023-10-14 14:08:38 -0400

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "axios";
+import { Dna } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function displayWeather(response) {
+    alert(
+      `The temperature in ${response.data.name} is ${response.data.main.temp}°C.`
+    );
+  }
+
+  let apiKey = "7ed26a6948c661d05fafe7355b41b2ec";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&units=metric&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(displayWeather);
+  return (
+    <Dna
+      visible={true}
+      height="80"
+      width="80"
+      ariaLabel="dna-loading"
+      wrapperStyle={{}}
+      wrapperClass="dna-wrapper"
+    />
+  );
+}
