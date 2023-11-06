fd7b57d2b411a48d03bbcda66a3c4b0ddb5fe31f
rochu36 <132965693+rochu36@users.noreply.github.com>
2023-11-05 23:59:12 -0500

    Updated 5-day forecast to start from tomorrow

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -23,7 +23,7 @@ export default function WeatherForecast(props) {
         <h2>5-Day Forecast</h2>
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
+            if (index > 0 && index < 6) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
